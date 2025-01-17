3ffd815d684105956700f6df2c4091c8af670175
verumeni <verumeni26@gmail.com>
2025-01-18 02:09:13 +0900

    プロジェクト追加 ユーザー定義例外クラス追加

---
+++ CSharpProject/Class/CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject.Class
+{
+    /// <summary>
+    /// ユーザー定義例外クラス
+    /// </summary>
+    public class CustomException : Exception
+    {
+        public CustomException() { }
+
+        public CustomException(string message) : base(message) { } 
+
+        public CustomException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ CSharpProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using CSharpProject.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //ユーザー定義の例外処理
+                throw new CustomException("ユーザー定義の例外");
+            }
+            catch (CustomException customEx)
+            {
+                Console.WriteLine("CustomException: " + customEx.Message);
+                try
+                {
+                    throw new Exception("Exception Throw");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
